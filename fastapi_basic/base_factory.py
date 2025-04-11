b01404cdd71e469d9f4052d09084a15f17f7d5d7
szx21023 <szx21023@gmail.com>
2025-04-11 11:05:53 +0800

    feat: add logger function

--- fastapi_basic/base_factory.py
+++ fastapi_basic/base_factory.py
@@ -3,10 +3,14 @@ from functools import lru_cache
 import os, dotenv
 
 from fastapi import FastAPI
+import logging
 
 from .utils import update_dict_with_cast
 
 class BaseFactory(metaclass=ABCMeta):
+    logger_name = 'my_log'
+    log_formatter = logging.Formatter('%(asctime)s %(filename)s %(levelname)s: %(message)s')
+
     @abstractmethod
     @lru_cache()
     def get_app_config(self):
@@ -22,8 +26,24 @@ class BaseFactory(metaclass=ABCMeta):
         app_config = self.get_app_config()
         app = FastAPI(docs_url=app_config.get('DOCS_URL'), redoc_url=app_config.get('REDOC_URL'), openapi_url=app_config.get('OPENAPI_URL'))
         app.state.config = app_config
+
+        self.__setup_main_logger(app, level=logging.DEBUG)
+
         return app
 
     def __load_local_config(self):
         dotenv.load_dotenv(dotenv_path='.env', override=True)
         update_dict_with_cast(self.get_app_config(), os.environ)
+
+    def __setup_main_logger(self, app, logger_name=logger_name, level=logging.DEBUG):
+        logger = self.__setup_logger(app, logger_name, level)
+        app.logger = logger
+
+    def __setup_logger(self, app, logger_name, level=logging.INFO):
+        logger = logging.getLogger(logger_name)
+        logger.setLevel(level)
+
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(self.log_formatter)
+        logger.addHandler(stream_handler)
+        return logger
