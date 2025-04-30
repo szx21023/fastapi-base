3a1eb6cbf0bd0cf69a6c05af897a3937f3c5ce9e
szx21023 <szx21023@gmail.com>
2025-04-30 09:27:58 +0800

    feat add function: reply_message, push_message

--- fastapi_basic/ext/line/__init__.py
+++ fastapi_basic/ext/line/__init__.py
@@ -1,4 +1,8 @@
 from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import LineBotApiError
+from linebot.models import TextMessage, TextSendMessage
+
+from ...exception.base_exception import LineBotException
 
 class LineBot:
     def __init__(self, channel_access_token: str, channel_secret: str):
@@ -6,3 +10,20 @@ class LineBot:
         self.channel_secret = channel_secret
         self.line_bot_api = LineBotApi(self.channel_access_token)
         self.handler = WebhookHandler(self.channel_secret)
+
+    async def reply_message(self, event, message):
+        try:
+            self.line_bot_api.reply_message(event.reply_token, TextMessage(text=message))
+
+        except LineBotApiError as err:
+            exception = LineBotException(str(err))
+            raise exception
+
+    async def push_message(self, line_uid: str, text: str):
+        try:
+            self.line_bot_api.push_message(line_uid, TextSendMessage(text=text))
+
+        except LineBotApiError as err:
+            message = f"Send line message fail, line_uid: {line_uid}, text: {text}, err: {str(err)}"
+            exception = LineBotException(message)
+            raise exception
