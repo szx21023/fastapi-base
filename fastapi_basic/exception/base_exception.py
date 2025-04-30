dc5e34fd9b5cb4ccaf6bf4cc87739d551a67dc5a
szx21023 <szx21023@gmail.com>
2025-04-30 09:25:58 +0800

    feat: add exception: LineBotException

--- fastapi_basic/exception/base_exception.py
+++ fastapi_basic/exception/base_exception.py
@@ -11,3 +11,13 @@ class InternalBaseException(HTTPException):
             "data": kwargs,
         }
         super().__init__(status_code=status_code, detail=detail)
+
+class LineBotException(HTTPException):
+    def __init__(self, status_code: int = status.HTTP_502_BAD_GATEWAY, code: str = "line_bot_error",
+                 message: str = "Line bot error", **kwargs):
+        detail = {
+            "code": code,
+            "message": message,
+            "data": kwargs,
+        }
+        super().__init__(status_code=status_code, detail=detail)
