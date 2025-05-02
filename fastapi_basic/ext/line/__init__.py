3deba2615533057c042d734a164e2f8243b2a5e2
szx21023 <szx21023@gmail.com>
2025-05-02 09:56:59 +0800

    feat: class LineBot add hander function

--- fastapi_basic/ext/line/__init__.py
+++ fastapi_basic/ext/line/__init__.py
@@ -1,15 +1,20 @@
+from abc import ABCMeta, abstractmethod
+import asyncio
+
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import LineBotApiError
-from linebot.models import TextMessage, TextSendMessage
+from linebot.models import TextMessage, TextSendMessage, MessageEvent
 
 from ...exception.base_exception import LineBotException
 
-class LineBot:
+class LineBot(metaclass=ABCMeta):
     def __init__(self, channel_access_token: str, channel_secret: str):
         self.channel_access_token = channel_access_token
         self.channel_secret = channel_secret
         self.line_bot_api = LineBotApi(self.channel_access_token)
         self.handler = WebhookHandler(self.channel_secret)
+
+        self.register_handlers()
 
     async def reply_message(self, event, text: str):
         try:
@@ -27,3 +32,16 @@ class LineBot:
             message = f"Send line message fail, line_uid: {line_uid}, text: {text}, err: {str(err)}"
             exception = LineBotException(message)
             raise exception
+
+    @abstractmethod
+    async def handle_message(self):
+        """
+        Each bot should define what handle_message it wants.
+        """
+
+    def register_handlers(self):
+        @self.handler.add(MessageEvent, message=TextMessage)
+        def handle_message(event: MessageEvent):
+            message = f'line_event: {event}'
+            self.logger.info(message)
+            asyncio.create_task(self.handle_message(event))
