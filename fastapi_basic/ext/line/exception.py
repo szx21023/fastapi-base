777a1a06d2bbaac0122867223b3c8c3e46a54049
szx21023 <szx21023@gmail.com>
2025-11-11 11:49:33 +0800

    fixed: change format of LineBotException

--- fastapi_basic/ext/line/exception.py
+++ fastapi_basic/ext/line/exception.py
@@ -1,13 +1,12 @@
-from fastapi import HTTPException
 from fastapi import status
+from ...exception.base_exception import InternalBaseException
 
 
-class LineBotException(HTTPException):
-    def __init__(self, status_code: int = status.HTTP_502_BAD_GATEWAY, code: str = "line_bot_error",
-                 message: str = "Line bot error", **kwargs):
-        detail = {
-            "code": code,
-            "message": message,
-            "data": kwargs,
-        }
-        super().__init__(status_code=status_code, detail=detail)
+class LineBotException(InternalBaseException):
+    def __init__(self, message: str = "", **kwargs):
+        status_code: int = status.HTTP_502_BAD_GATEWAY
+        code: str = "line_bot_error"
+        log_message: str = "Line bot error"
+
+        message = f"{log_message}, {message}" if message else log_message
+        super().__init__(status_code=status_code, code=code, message=message, **kwargs)
